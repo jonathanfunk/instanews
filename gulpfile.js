a1ca99fb0b0fa85b700105b1f403c4d455d90ac6
Jonathan Funk <jonfunk88@hotmail.com>
2016-10-13 20:34:28 -0700

    sass and gulp incorporated

--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,58 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync').create(),
     plumber = require('gulp-plumber'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
     eslint = require('gulp-eslint');
 
-gulp.task('uglify', function(){//Runs a task that we named default
-    gulp.src('./js/*.js')// Files gulp will work with
+    //sass
+    gulp.task('sass', function() {
+      gulp.src('./sass/style.scss')
+      .pipe(sass())
+      .pipe(autoprefixer({
+        browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
+    });
+
+    //uglify
+    gulp.task('js', function(){//Runs a task that we named default
+      gulp.src('./js/*.js')// Files gulp will work with
       .pipe(plumber())
       .pipe(uglify()) //Minifies
       .pipe(rename({ extname: '.min.js' })) //Changes name to .min.js
       .pipe(gulp.dest('./build/js')) //Takes everything and puts them in build/js?
-});
-
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
     });
 
-    gulp.watch("*.css").on('change', browserSync.reload);//Keep this inside browser-sync function
+    //browser-sync with watch nested inside
+    gulp.task('browser-sync', function() {
+      browserSync.init({
+        server: {
+          baseDir: "./"
+        }
+      });
 
-});
+      gulp.watch(["build/css/*.css", "build/js/*.js"]).on('change', browserSync.reload);
 
-gulp.task('lint', function() {
-  return gulp.src('js/*.js').pipe(eslint())
-  .pipe(eslint.format())
-  // Brick on failure to be super strict
-  .pipe(eslint.failOnError());
-});
+    });
 
+    //lint
+    gulp.task('lint', function() {
+      return gulp.src('js/*.js').pipe(eslint())
+      .pipe(eslint.format())
+      // Brick on failure to be super strict
+      .pipe(eslint.failOnError());
+    });
 
-gulp.task('default', ['browser-sync', 'uglify', 'lint']);
+    //watch
+    gulp.task('watch',function(){
+      gulp.watch('js/*js',['js','lint'])
+      gulp.watch('sass/*.scss',['sass']);
+    });
+
+    //default
+    gulp.task('default', ['browser-sync', 'watch']);
