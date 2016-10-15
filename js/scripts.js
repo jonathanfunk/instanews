6c91c43a5a122fcbb13e0d9a9168d9539b5167b8
Jonathan Funk <jonfunk88@hotmail.com>
2016-10-15 16:21:41 -0700

    animation implemented to header

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,9 @@
 $(document).ready(function(){
-  $('select').on('change',function(){
-    var curHeight = $('header').height();
-    $('header').css('height', 'auto');
-    var autoHeight = $('header').height();
-    var el = $('#first'),
-    curHeight = el.height(),
-    autoHeight = el.css('height', 'auto').height();
-    el.height(curHeight).animate({height: autoHeight}, 1000);
-    //$('header').height(curHeight).animate({height: autoHeight}, 1000);
 
-    $('.articles').slideUp('slow');
+  $('select').on('change',function(){
+    $('.logo img').css('height','110px');
+    $('header, .articles').animateAuto('height', 500);
+    $('header').height('auto');
   });
+
 });
